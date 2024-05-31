9e5826a56a65a04faec090866aab4f34afcdfbb5
o0sj0o <112302150+o0sj0o@users.noreply.github.com>
2024-05-31 11:00:54 +0300

    Add files via upload

--- src/components/BackButton/component.tsx
+++ src/components/BackButton/component.tsx
@@ -1,1 +1,23 @@
+import * as React from "react";
 
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+export interface BackButtonProps {
+  className?: string;
+  onClick(): void;
+}
+
+type Props = BackButtonProps;
+const BackButton: React.FC<Props> = ({ className, onClick }) => (
+  <IconButton
+    className={className}
+    color="inherit"
+    aria-label="Back"
+    onClick={onClick}
+  >
+    <ArrowBackIcon />
+  </IconButton>
+);
+
+export default BackButton;
